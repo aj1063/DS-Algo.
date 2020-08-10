1b73f987dc4c42baaadc7317c8fe7659e06282ea
aj1063 <aj5233448@gmail.com>
2020-08-10 09:55:53 +0530

    pattern

--- c++ Code/30.cpp
+++ c++ Code/30.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
-    cout<<"Hello Wojorld";
+{   int count=1;
+     //cout<<"enter the  value of n\n";
+     //cin>>n;   
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+
+
+             cout<<count<<" ";
+              count++;
+        }
+         cout<<endl;
+            
+
+
+    }
+    cout<<endl;
+   return 0;
 }
