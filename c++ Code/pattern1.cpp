d095dd25701284a188eb2f172d676389c3ee6ec2
aj1063 <aj5233448@gmail.com>
2020-08-10 10:20:12 +0530

    **pattern

---
+++ c++ Code/pattern1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int n;
+    cout<<"enter the value of n\n";
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            if(i>=j)
+                cout<<"*";
+            else
+            {
+                cout<<" ";
+            }
+                
+        }
+        cout<<endl;
+    }
+}
